260645bf989aa7b923fe0d512c58b6f34b6b841f
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-11 20:45:20 +0100

    implement createToken function

--- src/controllers/token.controller.ts
+++ src/controllers/token.controller.ts
@@ -1,14 +1,24 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
-import { Token } from "../entity/Token";
+import { Token, Platform } from "../entity/Token";
 import { v4 as uuidv4 } from 'uuid';
 
 export const createToken = async (req: Request, res: Response): Promise<Response> => {
+
+    const values:string[] = Object.values(Platform);
+    if(!values.includes(req.params.platform)){ 
+        return res.status(404).json({
+            message: "Unknown platform!"
+        });
+    }
     const newToken: Token = new Token();
-    newToken.token = uuidv4();
+    newToken.token = `${uuidv4()}:${req.params.platform}`;
+    newToken.platform = req.params.platform as Platform;
     newToken.remaining = 5;
+    await getRepository(Token).save(newToken);
     return res.status(200).json({
         token: newToken.token,
         remaining: newToken.remaining
     });
 }
+
