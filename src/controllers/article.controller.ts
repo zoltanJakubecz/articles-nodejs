5c8016b241d102ed5868e9cdf7999b498b66cbb9
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-11 15:58:46 +0100

    add status to response

--- src/controllers/article.controller.ts
+++ src/controllers/article.controller.ts
@@ -4,14 +4,14 @@ import { Article } from "../entity/Article";
 
 export const getArticles = async (req: Request, res: Response): Promise<Response> => {
     const articles = await getRepository(Article).find();
-    return res.json(articles);
+    return res.status(200).json(articles);
 }
 
 
 export const createArticle = async (req: Request, res: Response): Promise<Response> => {
     const newArticle = await getRepository(Article).create(req.body);
     const result = await getRepository(Article).save(newArticle);
-    return res.json(result);
+    return res.status(200).json(result);
 }
 
 
