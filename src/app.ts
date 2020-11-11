36d9a39ee2ebf44ab9b44cd4d072f577dc295475
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-11 10:12:54 +0100

    WIP

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import express from 'express';
+import { getArticles } from './controllers/article.controller';
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/', getArticles);
 
 createConnection().then(() => {
     app.listen(5000, () =>{
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {createConnection} from "typeorm";
 
 createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
+    // console.log("Inserting a new user into the database...");
     // const user = new User();
     // user.firstName = "Timber";
     // user.lastName = "Saw";
@@ -15,6 +15,6 @@ createConnection().then(async connection => {
     // const users = await connection.manager.find(User);
     // console.log("Loaded users: ", users);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+    // console.log("Here you can setup and run express/koa/any other framework.");
 
 }).catch(error => console.log(error));
