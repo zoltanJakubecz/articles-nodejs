b8f4cad1810e2d0532e97ea47e96436595f3dd1c
Zoltan Jakubecz <jakuzo@gmail.com>
2020-11-11 14:42:28 +0100

    fix file upload response

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,16 +1,18 @@
 import { Request, Response } from "express-fileupload";
 import path from "path";
 
+const baseUrl = process.env.BASE_URL || "http://localhost:5000";
+
 export const uploadFile = (req: Request, res: Response): Promise<Response> => {
     try {
         const file = req.files.file;
-        const fileName = file.name;
+        const fileName = `${Date.now()}-${file.name}`;
         const extension = path.extname(fileName);
         const allowedExtensions = /png|jpeg|jpg|gif/;
 
         if(!allowedExtensions.test(extension)) throw "Unsupported extension";
-        console.log(extension);
-        return res.send("Helly");
+        file.mv("./public/imgs/" + fileName);
+        return res.status(200).send(`${baseUrl}/imgs/${fileName}`);
         
     } catch (error) {
         console.log(error);
